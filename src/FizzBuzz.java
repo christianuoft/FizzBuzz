3444c454170cb510c915ac2fd24147a25e7f211c
Christian <71178988+christianjokhaji@users.noreply.github.com>
2024-05-17 13:24:38 -0400

    refactored to extract helper method

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -5,30 +5,54 @@ class FizzBuzz {
 
     public static void main(String[] args) {
 
-        for (int i = 1; i < 100; i++) {
+//        for (int i = 1; i < 100; i++) {
+//
+//            // Find out which numbers divide i.
+//            boolean divisibleBy3 = i % 3 == 0;
+//            boolean divisibleBy5 = i % 5 == 0;
+//
+//            // Print our appropriate result.
+//            if (divisibleBy3 && divisibleBy5) {
+//
+//                System.out.println("Fizz Buzz");
+//
+//            } else if (divisibleBy3) {
+//
+//                System.out.println("Fizz");
+//
+//            } else if (divisibleBy5) {
+//
+//                System.out.println("Buzz");
+//
+//            } else {
+//
+//                System.out.println(i);
+//
+//            }
+//        }
 
-            // Find out which numbers divide i.
+        int i = 1;
+        while (i < 100) {
             boolean divisibleBy3 = i % 3 == 0;
             boolean divisibleBy5 = i % 5 == 0;
 
-            // Print our appropriate result.
-            if (divisibleBy3 && divisibleBy5) {
+            doFizzBuzz(divisibleBy3, divisibleBy5, i);
+            i += 1;
+        }
+    }
 
-                System.out.println("Fizz Buzz");
-
-            } else if (divisibleBy3) {
-
-                System.out.println("Fizz");
-
-            } else if (divisibleBy5) {
-
-                System.out.println("Buzz");
-
-            } else {
-
-                System.out.println(i);
-
-            }
+    private static void doFizzBuzz(boolean divisibleBy3, boolean divisibleBy5, int i) {
+        if (divisibleBy3 && divisibleBy5) {
+            System.out.println("Fizz Buzz");
+        }
+        else if (divisibleBy3) {
+            System.out.println("Fizz");
+        }
+        else if (divisibleBy5) {
+            System.out.println("Buzz");
+        }
+        else {
+            System.out.println(i);
         }
     }
 }
